b33098b1eb1e9d73e81116c36749b6e0516aa3cd
Alex Curran <haletas88@gmail.com>
2025-10-03 12:04:05 +1300

    Fix issues

--- src/gui.cpp
+++ src/gui.cpp
@@ -104,9 +104,9 @@ void Gui::DrawProject(const std::string &projectName, const std::string &project
 
         std::filesystem::create_directory(projectName);
 
-        system(("cmake -S . -B " + projectName + " -G Ninja -DGAME=ON -DSCRIPTS=" + projectPath + "/scripts").c_str());
+        system(("cmake -S . -B " + projectPath + "/" + projectName + " -G Ninja -DGAME=ON -DSCRIPTS=" + projectPath + "/scripts").c_str());
 
-        std::filesystem::current_path(std::filesystem::current_path() / projectName);
+        std::filesystem::current_path(projectPath + "/" + projectName);
 
         system("ninja");
 
